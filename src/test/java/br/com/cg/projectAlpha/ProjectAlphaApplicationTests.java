d42ca381d61ef47fbe1de18d74b980de0085b352
Tiago Roberto <tiagowebwork@gmail.com>
2023-10-28 04:04:44 -0300

    first commit

---
+++ src/test/java/br/com/cg/projectAlpha/ProjectAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cg.projectAlpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
